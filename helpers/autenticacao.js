59663e6c11900580f54432cefd4c748418c308cc
Michelle <michelleyonezawa@gmail.com>
2025-11-03 14:24:56 -0300

    Adicionando fixtures para gerar corpos das requisições

--- helpers/autenticacao.js
+++ helpers/autenticacao.js
@@ -1,13 +1,12 @@
 const request = require('supertest');
+const postLogin = require('../fixtures/postLogin.json');
 
 const obterToken = async (usuario, senha) => {
+    const bodyLogin = { ...postLogin }
     const respostaLogin = await request(process.env.BASE_URL)
         .post('/api/users/login')
         .set('Content-Type', 'application/json')
-        .send({
-            username: usuario,
-            password: senha
-        });
+        .send(bodyLogin);
 
     return respostaLogin.body.token;
 }
--- test/shift.test.js
+++ test/shift.test.js
@@ -2,6 +2,7 @@ const request = require('supertest');
 const { expect } = require('chai');
 require('dotenv').config()
 const { obterToken } = require('../helpers/autenticacao');
+const getShifts = require('../fixtures/getShifts.json')
 
 describe('Registrar plantão', () => {
     describe('POST /shifts', () => {
@@ -12,35 +13,26 @@ describe('Registrar plantão', () => {
         })
 
         it('Deve retornar 201 quando registrar um novo plantão com sucesso', async () => {
+        const bodyShifts = { ...getShifts}
 
             const resposta = await request(process.env.BASE_URL)
                 .post('/api/shifts')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
-                .send({
-                    date: "2025-11-03",
-                    value: 100,
-                    location: "Hospital",
-                    startTime: "07:00",
-                    endTime: "13:00"
-                });
+                .send(bodyShifts);
 
             expect(resposta.status).to.equal(201);
         });
 
         it('Deve retornar 400 quando registrar um novo plantão com dados inválidos', async () => {
+            const bodyShifts = { ...getShifts }
+            bodyShifts.date = "2025-11-100";
 
             const resposta = await request(process.env.BASE_URL)
                 .post('/api/shifts')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
-                .send({
-                    date: "2025-11-100",
-                    value: 100,
-                    location: "Hospital",
-                    startTime: "30:00",
-                    endTime: "13:00"
-                });
+                .send(bodyShifts);
 
             expect(resposta.status).to.equal(400);
         });
